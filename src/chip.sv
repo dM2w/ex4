1ddbb74325b5a4c12103a6174c3097b20e701a21
hotpotyy <sylviayao0402@gmail.com>
2025-02-13 15:02:27 -0500

    change chip.sv

--- src/chip.sv
+++ src/chip.sv
@@ -11,38 +11,59 @@ module my_chip (
     // TODO: remove the counter design and use this module to insert your own design
     // DO NOT change the I/O header of this design
 
-    wire [6:0] led_out;
-    assign io_out[6:0] = led_out;
+    // wire [6:0] led_out;
+    // assign io_out[6:0] = led_out;
 
-    // external clock is 1000Hz, so need 10 bit counter
-    reg [9:0] second_counter;
-    reg [3:0] digit;
+    logic go, finish;
+    logic [9:0] data_in;
+    assign io_in[0] = go;
+    assign io_in[1] = finish;
+    assign io_in[11:2] = data_in;
+   
+    logic debug_error;
+    logic [9:0] range;
+    assign io_out[0] = debug_error;
+    assign io_out[10:1] = range;
+ 
+    logic [9:0] max, min;
+    logic going;
 
-    always @(posedge clock) begin
-        // if reset, set counter to 0
+    always_ff @(posedge clock, posedge reset) begin
         if (reset) begin
-            second_counter <= 0;
-            digit <= 0;
+            debug_error <= 0;
+            max <= 0;
+            min <= {10{'b1}};
+            range <= 0;
+            going <= 0;
+            
         end else begin
-            // if up to 16e6
-            if (second_counter == 1000) begin
-                // reset
-                second_counter <= 0;
+            if (go && finish) begin
+                debug_error <= 1;
+            end else if (!going && finish) begin
+                debug_error <= 1;
+            end else if (go) begin
+                debug_error <= 0;
+            end
 
-                // increment digit
-                digit <= digit + 1'b1;
-
-                // only count from 0 to 9
-                if (digit == 9)
-                    digit <= 0;
-
-            end else
-                // increment counter
-                second_counter <= second_counter + 1'b1;
+            if (go && !going) begin
+                // transist to find range
+                going <= 1;
+                max <= data_in;
+                min <= data_in;
+            end else if (going) begin
+                if (finish) begin
+                    range <= max - min;
+                    going <= 0;
+                end else begin
+                    if (data_in > max) begin
+                        max <= data_in;
+                    end
+                    if (data_in < min) begin
+                        min <= data_in;
+                    end
+                end
+            end
         end
     end
-
-    // instantiate segment display
-    seg7 seg7(.counter(digit), .segments(led_out));
 
 endmodule
